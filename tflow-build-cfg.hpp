576d4d229c3a6d6ce348c31b7584090a54496344
Andrejs Vasiljevs <av@reedl.tech>
2025-05-04 19:52:20 +0300

    sync

--- tflow-build-cfg.hpp
+++ tflow-build-cfg.hpp
@@ -4,7 +4,4 @@
 #define GFTT_MT 1
 #define TFLOW_PROCESS_CPU_NUM 2		// NOTE: CPU affinity is set inside the class constructor.
 #define TFLOW_GFTT_MT_CPU_NUM 3		// Thus, can be part of module's configuration 
-
-#define SRC_PLAYER 0
-#define SRC_CAM 1
-									
+								
